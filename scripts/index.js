50425b20b89f0d4979eb69704d79d6bad7497c1d
Javier Vera Flores <aczayacatl27@hotmail.com>
2025-01-19 15:04:50 -0600

    terminando cards de home page

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,16 @@
 //mandaremos un mensaje por el DOM
 const divMain = document.querySelector("#div-main");
+divMain.innerHTML = "Hola desde el DOM";
+divMain.set;
 const titulo = document.createElement("p");
-titulo.innerHTML = "Titulo desde el DOM";
+
+const ObtenerHoraCDMX = () => {
+  let fecha = new Date();
+  const hora = fecha.getHours().toString().padStart(2, "0");
+  const minutos = fecha.getMinutes().toString().padStart(2, "0");
+  const segundos = fecha.getSeconds().toString().padStart(2, "0");
+  console.log(`La hora actual en CDMX es: ${hora}: ${minutos}:${segundos}`);
+  titulo.innerHTML = `La hora actual en CDMX es: ${hora}: ${minutos}:${segundos}`;
+};
 divMain.appendChild(titulo);
+setInterval(ObtenerHoraCDMX, 1000);
